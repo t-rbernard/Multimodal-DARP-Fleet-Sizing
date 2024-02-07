bc6394ed58201a80f4bd6d195807dd5c60e2a9a5
Romain BERNARD <romain.bernard@uca.fr>
2024-02-07 18:53:41 +0100

    add dat file parsing to create a graph

--- src/instance/graph/Graph.cpp
+++ src/instance/graph/Graph.cpp
@@ -2,6 +2,12 @@
 // Created by rbernard on 22/01/24.
 //
 
+#ifdef DEBUG
+#define DEBUG_MSG(str) do { std::cout << str << std::endl; } while( false )
+#else
+#define DEBUG_MSG(str) do { } while ( false )
+#endif
+
 #include <fstream>
 #include <charconv>
 #include <random>
@@ -9,7 +15,6 @@
 #include "Graph.h"
 #include "Node.h"
 #include "../../services/CSV/CSVRange.h"
-#include "../../services/DatFile/DATRow.h"
 
 Graph::Graph(std::string nodesFilePath, std::string edgesFilePath, std::string ptLinesFilePath) {
 
@@ -27,7 +32,7 @@ Graph::Graph(std::string nodesFilePath, std::string edgesFilePath, std::string p
             std::from_chars(row[1].data(), row[1].data() + row[1].size(), x);
             std::from_chars(row[2].data(), row[2].data() + row[2].size(), y);
             this->nodesVector.emplace_back(Status::work, x, y);
-            std::cout << "Created new node " << x << " " << y << std::endl;
+            DEBUG_MSG("Created new node " << x << " " << y);
         }
     }
 
@@ -46,7 +51,7 @@ Graph::Graph(std::string nodesFilePath, std::string edgesFilePath, std::string p
             std::from_chars(row[1].data(), row[1].data() + row[1].size(), edge_end);
             std::from_chars(row[2].data(), row[2].data() + row[2].size(), length);
             this->edgesVector.emplace_back(edge_start, edge_end, length);
-            std::cout << "Created new edge between " << edge_start << " and " << edge_end << " of length " << length << std::endl;
+            DEBUG_MSG("Created new edge between " << edge_start << " and " << edge_end << " of length " << length);
         }
     }
 
@@ -100,7 +105,7 @@ Graph::Graph(std::string nodesFilePath, std::string edgesFilePath, std::string p
                 newTimetable.clear();
             }
 
-            std::cout << "Created new line with nodes" << std::endl;
+            DEBUG_MSG("Created new line with nodes");
         }
     }
     std::cout << "test is done" << std::endl;
@@ -114,25 +119,83 @@ Graph::Graph(const std::string& datFilePath) {
     //-- Read params
     infile >> currentRow;
     std::cout << currentRow[0] << std::endl;
-    infile >> currentRow; // read seed
-    const std::string rngSeed = currentLine;
+    // Seeded random number generator
+    infile >> currentRow;
+    unsigned long rngSeed;
+    std::from_chars(currentRow[1].data(), currentRow[1].data() + currentRow[1].length(), rngSeed);
+    auto rng = std::mt19937(rngSeed);
+
     //-- End of params
 
     //-- Read nodes
     infile >> currentRow; // Read and print comment line for format
-    std::cout << currentRow[0] << std::endl;
-    while(infile >> currentRow && currentRow[0].starts_with('#')) {
+    std::cout << currentRow.toString() << std::endl;
+    std::string status; double x,y;
+    while(infile >> currentRow && !currentRow[0].starts_with('#')) {
+        status = currentRow[0]; //TODO:parse status enum value from string
+        std::from_chars(currentRow[1].data(), currentRow[1].data() + currentRow[1].length(), x);
+        std::from_chars(currentRow[2].data(), currentRow[2].data() + currentRow[2].length(), y);
 
+        this->nodesVector.emplace_back(Status::work, x, y);
+        DEBUG_MSG("Created new node " << x << " " << y << " with status = " << status);
     }
     //-- End of nodes
 
     //-- Read Edges
-    std::cout << currentRow[0] << std::endl;
-    while(infile >> currentLine) {
+    std::cout << currentRow.toString() << std::endl;
+    int og_idx, dest_idx; double length;
+    while(infile >> currentRow && !currentRow[0].starts_with('#')) {
+        std::from_chars(currentRow[0].data(), currentRow[0].data() + currentRow[0].length(), og_idx);
+        std::from_chars(currentRow[1].data(), currentRow[1].data() + currentRow[1].length(), dest_idx);
+        std::from_chars(currentRow[2].data(), currentRow[2].data() + currentRow[2].length(), length);
 
+        this->edgesVector.emplace_back(og_idx, dest_idx, length);
+        DEBUG_MSG("Created new edge between " << og_idx << " and " << dest_idx << " of length " << length);
     }
     //-- End of edges
 
     //-- Read Public transit line
-    std::cout << currentRow[0] << std::endl;
+    std::cout << currentRow.toString() << std::endl;
+    int startTime, endTime, frequency, currentNodeIdx;
+    std::uniform_int_distribution<uint32_t> uint_dist10(1,10);
+    std::uniform_int_distribution<uint32_t> uint_dist60(1,60);
+    while(infile >> currentRow && !currentRow[0].starts_with('#')) {
+        //add nodes for the line
+        Line newLine = Line();
+
+        //Create a base timetable. It'll be used as a basis to generate subsequent nodes' timetables
+        std::vector<int> timeTable;
+        std::from_chars(currentRow[0].data(), currentRow[0].data() + currentRow[0].size(), frequency);
+        std::from_chars(currentRow[1].data(), currentRow[1].data() + currentRow[1].size(), startTime);
+        std::from_chars(currentRow[2].data(), currentRow[2].data() + currentRow[3].size(), endTime);
+        int currentTime = startTime + uint_dist60(rng); //random startTime time with 60min max offset
+        while(currentTime + frequency < endTime)
+        {
+            timeTable.push_back(currentTime + frequency);
+            currentTime += frequency;
+        }
+        newLine.addTimetable(timeTable);
+
+        for(int i = 3; i < currentRow.size(); ++i)
+        {
+            std::from_chars(currentRow[i].data(), currentRow[i].data() + currentRow[i].size(), currentNodeIdx);
+            newLine.addNode(currentNodeIdx);
+        }
+
+        //Create subsequent timetables according to preceding timetable and travel time
+        for(int i = 1; i < newLine.getNodes().size(); ++i)
+        {
+            int travelTime = uint_dist10(rng); //FIXME travel time is randomized for now, we should get edge length if it exists I guess
+            std::vector<int> precedingTimeTable = newLine.getTimetable(i - 1);
+            std::vector<int> newTimetable;
+            for(auto it = precedingTimeTable.begin(); it != precedingTimeTable.end(); ++it)
+            {
+                newTimetable.emplace_back(*it.base() + travelTime);
+            }
+            newLine.addTimetable(newTimetable);
+            newTimetable.clear();
+        }
+
+        DEBUG_MSG("Created new line with nodes");
+    }
 }
--- test/debug.cpp
+++ test/debug.cpp
@@ -5,8 +5,10 @@
 #include "../src/instance/graph/Graph.h"
 
 int main() {
-    Graph give_me_a_name("../resources/test/instances/basic_debug_instance/nodes.csv",
-          "../resources/test/instances/basic_debug_instance/edges.csv",
-          "../resources/test/instances/basic_debug_instance/PT_lines.csv");
+//    Graph give_me_a_name("../resources/test/instances/basic_debug_instance/nodes.csv",
+//          "../resources/test/instances/basic_debug_instance/edges.csv",
+//          "../resources/test/instances/basic_debug_instance/PT_lines.csv");
+
+    Graph give_me_another_name("../resources/test/instances/basic_debug_instance/graph.dat");
     return 0;
 }
