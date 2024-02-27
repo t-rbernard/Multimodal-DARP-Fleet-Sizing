a25bbf27bf0a77592b05362033d5e7b30ac8c320
Romain BERNARD <30secondstodraw@gmail.com>
2024-02-27 02:34:30 +0100

    add new debug instances to test transit algorithm development

--- test/debug.cpp
+++ test/debug.cpp
@@ -4,18 +4,25 @@
 #include <iostream>
 #include "../src/instance/graph/Graph.h"
 #include "../src/ShortestPath/Transit/TransitStateContainer.h"
+#include "../src/ShortestPath/Transit/TransitShortestPathPrecompute.h"
 
 int main() {
 //    Graph give_me_a_name("../resources/test/instances/basic_debug_instance/nodes.csv",
 //          "../resources/test/instances/basic_debug_instance/edges.csv",
 //          "../resources/test/instances/basic_debug_instance/PT_lines.csv");
 
-    Graph graphFromSingleFile("../resources/test/instances/basic_debug_instance/graph.dat");
-    graphFromSingleFile.exportGraphToFiles("../resources/test/outputs/basic_debug_instance/");
-    TransitStateContainer stateContainer;
-    stateContainer.resizeSolutionsVector(graphFromSingleFile.getNodesVector().size());
-    stateContainer.pushEmptyState(3);
-    stateContainer.getSolutions(3);
-    stateContainer.getBestSolution(3);
+//    std::string instanceFolder = "basic_debug_instance/";
+    std::string instanceFolder = "two_lines_debug_instance/";
+    std::string datFile = "graph.dat";
+
+    Graph graphFromSingleFile("../resources/test/instances/" + instanceFolder + datFile);
+    graphFromSingleFile.exportGraphToFiles("../resources/test/outputs/" + instanceFolder);
+    for(auto& ptLine : graphFromSingleFile.getPTLines()) {
+        for(int i = 0; i < ptLine.getNodes().size(); ++i) {
+            for (auto& startingTime: ptLine.getTimetable(i)) {
+                TransitShortestPathPrecompute::executeAlgorithm(graphFromSingleFile, ptLine.getNode(i),startingTime);
+            }
+        }
+    }
     return 0;
 }
