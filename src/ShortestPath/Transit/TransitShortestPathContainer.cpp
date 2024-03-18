5992e04715b3e2ffe727ce15561d0729d3e8e56b
Romain BERNARD <romain.bernard@uca.fr>
2024-03-18 18:30:24 +0100

    shortest path container implementation (TODO : tests)

--- src/ShortestPath/Transit/TransitShortestPathContainer.cpp
+++ src/ShortestPath/Transit/TransitShortestPathContainer.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "TransitShortestPathContainer.h"
+
+void TransitShortestPathContainer::addShortestPathCollection(int startNodeIndex,
+                                                             const std::pair<int, std::vector<TransitShortestPath>>& shortestPathList) {
+    container.at(startNodeIndex).emplace_back(shortestPathList);
+}
+
+void TransitShortestPathContainer::addShortestPathCollection(int startNodeIndex, int startingInstant, int graphSize,
+                                                             const std::vector<TransitAlgorithmState>& algorithmResultStates) {
+    std::vector<TransitShortestPath> shortestPathList;
+    shortestPathList.reserve(graphSize);
+
+    //Convert states to shortest paths and add to collection
+    for(const auto& state: algorithmResultStates) {
+        shortestPathList.emplace_back(state);
+    }
+
+    //Add the (startingInstant, pathVector) pair at the appropriate node index
+    container.at(startNodeIndex).emplace_back(startingInstant, shortestPathList);
+}
+
+std::vector<std::pair<int, std::vector<TransitShortestPath>>>::iterator
+TransitShortestPathContainer::getShortestPathsFromTime(int startNodeIndex, int earliestStartInstant) {
+    const auto& iterator = std::lower_bound(container.at(startNodeIndex).begin(), container.at(startNodeIndex).end(),
+                                            std::pair<int, std::vector<TransitShortestPath>>(earliestStartInstant, {}));
+
+    return iterator;
+}
+
+std::pair<int, TransitShortestPath>
+TransitShortestPathContainer::getShortestPathToYFromTime(int startNodeIndex, int earliestStartInstant, int goalNode) {
+    const auto& shortestPathsIterator = getShortestPathsFromTime(startNodeIndex, earliestStartInstant);
+    return std::pair(shortestPathsIterator->first, shortestPathsIterator->second.at(goalNode));
+}
--- src/ShortestPath/Transit/TransitShortestPathContainer.h
+++ src/ShortestPath/Transit/TransitShortestPathContainer.h
@@ -11,12 +11,13 @@
 
 class TransitShortestPathContainer {
 private:
-    std::vector<std::pair<int, std::vector<TransitShortestPath>>> container; //Node vector -> Pair<Instant, ShortestPath[Node]>
+    std::vector<std::vector<std::pair<int, std::vector<TransitShortestPath>>>> container; //NodeVector< PairVector<Pair<Instant, NodeVector<ShortestPath> >> >
 
 public:
-    void addShortestPathCollection(int startNodeIndex, std::pair<int, std::vector<TransitShortestPath>> shortestPathList);
-    void addShortestPathCollection(int startNodeIndex, int startingInstant, std::vector<TransitAlgorithmState> algorithmResultStates);
-    std::pair<int, std::vector<TransitShortestPath>> getShortestPathsFromTime(int startNodeIndex, int );
+    void addShortestPathCollection(int startNodeIndex, const std::pair<int, std::vector<TransitShortestPath>>& shortestPathList);
+    void addShortestPathCollection(int startNodeIndex, int startingInstant, int graphSize, const std::vector<TransitAlgorithmState>& algorithmResultStates);
+    std::vector<std::pair<int, std::vector<TransitShortestPath>>>::iterator getShortestPathsFromTime(int startNodeIndex, int earliestStartInstant);
+    std::pair<int, TransitShortestPath> getShortestPathToYFromTime(int startNodeIndex, int earliestStartInstant, int goalNode);
 
 };
 
