b87e0b38962eca85e08d5438d4f51c3c85707434
Romain BERNARD <romain.bernard@uca.fr>
2024-03-22 16:53:31 +0100

    Add new data to Instance

--- src/instance/Instance.h
+++ src/instance/Instance.h
@@ -4,18 +4,25 @@
 
 #ifndef GREEDYALGORITHM_INSTANCE_H
 #define GREEDYALGORITHM_INSTANCE_H
+#include <utility>
 #include <vector>
 #include "requests/Request.h"
 #include "graph/Graph.h"
+#include "SAEVehicle.h"
 
 class Instance {
 private:
     std::vector<Request> requests;
     Graph graph;
+    int vehicleCapacity;
 
 public:
+    Instance(const std::vector<Request> &requests, Graph graph, const int vehicleCapacity) : requests(
+            requests), graph(std::move(graph)), vehicleCapacity(vehicleCapacity) {}
+
     [[nodiscard]] Graph const & getGraph() const { return graph;}
     [[nodiscard]] std::vector<Request> const & getRequests() const { return requests;}
+    [[nodiscard]] int getVehicleCapacity() const { return vehicleCapacity; }
 };
 
 
