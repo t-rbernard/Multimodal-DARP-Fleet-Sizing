de1eb1f337b7ae17eacb66fc555b58c99f626ca3
Romain BERNARD <30secondstodraw@gmail.com>
2024-05-24 04:35:46 +0200

    instance constructor create graph and parse requests file

--- src/instance/Instance.cpp
+++ src/instance/Instance.cpp
@@ -2,6 +2,24 @@
 // Created by rbernard on 22/01/24.
 //
 
+#include <iostream>
 #include "Instance.h"
 #include "graph/Graph.h"
 #include "requests/Request.h"
+#include "../services/DatFile/DATRow.h"
+
+Instance::Instance(const std::string& graphFilePath,
+                   const std::string& requestsFilePath,
+                   const int vehicleCapacity): _graph(graphFilePath),_vehicleCapacity(vehicleCapacity)
+{
+    parseRequestsFromFile(requestsFilePath);
+}
+
+void Instance::parseRequestsFromFile(const std::string &requestsFilePath) {
+    std::ifstream infile(requestsFilePath);
+    DATRow currentRow = DATRow(',');
+    std::string currentLine;
+    while(infile >> currentRow && !currentRow[0].starts_with('#')) {
+        _requests.emplace_back(currentRow, _graph);
+    }
+}
--- src/instance/Instance.h
+++ src/instance/Instance.h
@@ -12,17 +12,21 @@
 
 class Instance {
 private:
-    std::vector<Request> requests;
-    Graph graph;
-    int vehicleCapacity;
+
+    std::vector<Request> _requests;
+    Graph _graph;
+    int _vehicleCapacity;
 
 public:
-    Instance(const std::vector<Request> &requests, Graph graph, const int vehicleCapacity) : requests(
-            requests), graph(std::move(graph)), vehicleCapacity(vehicleCapacity) {}
+    Instance(const std::string &graphFilePath, const std::string &requestsFilePath, const int vehicleCapacity);
+    Instance(const std::vector<Request> &requests, Graph graph, const int vehicleCapacity) : _requests(
+            requests), _graph(std::move(graph)), _vehicleCapacity(vehicleCapacity) {}
 
-    [[nodiscard]] Graph const & getGraph() const { return graph;}
-    [[nodiscard]] std::vector<Request> const & getRequests() const { return requests;}
-    [[nodiscard]] int getVehicleCapacity() const { return vehicleCapacity; }
+    [[nodiscard]] Graph const & getGraph() const { return _graph;}
+    [[nodiscard]] std::vector<Request> const & getRequests() const { return _requests;}
+    [[nodiscard]] int getVehicleCapacity() const { return _vehicleCapacity; }
+
+    void parseRequestsFromFile(const std::string &basicString);
 };
 
 
