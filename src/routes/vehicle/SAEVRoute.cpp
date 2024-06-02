be2703f9aca03ec96ebae4cf6a7f96c4c8d3d59d
Romain BERNARD <30secondstodraw@gmail.com>
2024-06-02 16:49:37 +0200

    route insertion and initialization fix

--- src/routes/vehicle/SAEVRoute.cpp
+++ src/routes/vehicle/SAEVRoute.cpp
@@ -6,7 +6,7 @@
 #include "SAEVRoute.h"
 
 SAEVRoute::SAEVRoute(const Graph& graph, const std::vector<Request>& requestList) : _nbRequest(requestList.size()), _graph(&graph), _requestList(&requestList) {
-    _route.reserve(_nbRequest*4); //nbRequest*2 O/D KeyPoints + nbRequest*2 Start/End depots (upper bound #vehicle = #requests
+    _route.resize(_nbRequest*4); //nbRequest*2 O/D KeyPoints + nbRequest*2 Start/End depots (upper bound #vehicle = #requests
 
     //Init Request O/D and Depot start/end key points
     for(size_t i = 0; i < _nbRequest; ++i) {
@@ -23,29 +23,43 @@ SAEVRoute::SAEVRoute(const Graph& graph, const std::vector<Request>& requestList
         //Link Origin depots and Destination depots
         getOriginDepot(i).setCounterpart(&getDestinationDepot(i));
         getDestinationDepot(i).setCounterpart(&getOriginDepot(i));
+        getOriginDepot(i).setSuccessor(&getDestinationDepot(i));
+        getDestinationDepot(i).setPredecessor(&getOriginDepot(i));
     }
 }
 
 void SAEVRoute::insertRequest(int requestIdx, int originRequestPredecessorIdx, int destinationRequestPredecessorIdx) {
-    SAEVKeyPoint& originKp = _route.at(requestIdx);
-    SAEVKeyPoint& destinationKp = _route.at(requestIdx + 1);
+    SAEVKeyPoint& originKp = getOrigin(requestIdx);
+    SAEVKeyPoint& destinationKp = getDestination(requestIdx);
 
     SAEVKeyPoint& originPredKp = _route.at(originRequestPredecessorIdx);
     SAEVKeyPoint *originSuccKp = originPredKp.getSuccessor();
     SAEVKeyPoint& destinationPredKp = _route.at(destinationRequestPredecessorIdx);
     SAEVKeyPoint *destinationSuccKp = destinationPredKp.getSuccessor();
 
-    //Set values for O/D KPs
-    originKp.setPredecessor(&originPredKp);
-    originKp.setSuccessor(originSuccKp);
-    destinationKp.setPredecessor(&destinationPredKp);
-    destinationKp.setSuccessor(destinationSuccKp);
+    if(originRequestPredecessorIdx != destinationRequestPredecessorIdx) {
+        //Set values for O/D KPs
+        originKp.setPredecessor(&originPredKp);
+        originKp.setSuccessor(originSuccKp);
+        destinationKp.setPredecessor(&destinationPredKp);
+        destinationKp.setSuccessor(destinationSuccKp);
 
-    //Set values for predecessors/successors
-    originPredKp.setSuccessor(&originKp);
-    originSuccKp->setPredecessor(&originKp);
-    destinationPredKp.setSuccessor(&destinationKp);
-    destinationSuccKp->setPredecessor(&destinationKp);
+        //Set values for predecessors/successors
+        originPredKp.setSuccessor(&originKp);
+        originSuccKp->setPredecessor(&originKp);
+        destinationPredKp.setSuccessor(&destinationKp);
+        destinationSuccKp->setPredecessor(&destinationKp);
+    } else {
+        //Set values for O/D KPs
+        originKp.setPredecessor(&originPredKp);
+        originKp.setSuccessor(&destinationKp);
+        destinationKp.setPredecessor(&originKp);
+        destinationKp.setSuccessor(destinationSuccKp);
+
+        //Set values for predecessors/successors
+        originPredKp.setSuccessor(&originKp);
+        originSuccKp->setPredecessor(&destinationKp);
+    }
 }
 
 void SAEVRoute::removeRequest(int requestIdx) {
