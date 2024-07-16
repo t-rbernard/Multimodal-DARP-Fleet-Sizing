778ae10aa77710f99b8a2f53f5f9a85d27526e2b
Romain BERNARD <romain.bernard@uca.fr>
2024-07-16 17:27:12 +0200

    remove unused classes and functions (formatting results is handled via TransitShortestPathContainer serialization methods)

--- src/ShortestPath/Transit/TransitShortestPathPrecompute.h
+++ src/ShortestPath/Transit/TransitShortestPathPrecompute.h
@@ -5,14 +5,12 @@
 #ifndef GREEDYALGORITHM_TRANSITSHORTESTPATHPRECOMPUTE_H
 #define GREEDYALGORITHM_TRANSITSHORTESTPATHPRECOMPUTE_H
 
-#include "../TimeDependentShortestPathContainer.h"
 #include "TransitShortestPath.h"
 #include "../../instance/graph/Graph.h"
 #include "TransitStateContainer.h"
 
 class TransitShortestPathPrecompute {
 public:
-    static ShortestPathContainer<TransitShortestPath> formatResults(TimeDependentShortestPathContainer<TransitShortestPath> container);
     static TransitStateContainer executeAlgorithm(const Graph& graph, int nodeIndex, int instant);
 };
 
