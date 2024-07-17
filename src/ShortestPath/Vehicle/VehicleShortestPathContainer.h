aa874bb26ee7b273c188767593110109b75dc7d0
Romain BERNARD <romain.bernard@uca.fr>
2024-07-17 18:31:27 +0200

    update container API and docs

--- src/ShortestPath/Vehicle/VehicleShortestPathContainer.h
+++ src/ShortestPath/Vehicle/VehicleShortestPathContainer.h
@@ -15,7 +15,14 @@ private:
     std::vector<std::vector<uint>> _distanceMatrix{};
 public:
     VehicleShortestPathContainer() = default;
-    VehicleShortestPathContainer(size_t matrixSize) {
+
+    /**
+     * Moves the given shortestPathsMatrix to initialize the container with the given distances.
+     * @param shortestPathsMatrix
+     */
+    explicit VehicleShortestPathContainer(std::vector<std::vector<uint>> shortestPathsMatrix) : _distanceMatrix(std::move(shortestPathsMatrix)) {}
+
+    explicit VehicleShortestPathContainer(size_t matrixSize) {
         _distanceMatrix.resize(matrixSize);
         for(int i = 0; i < _distanceMatrix.capacity(); ++i)
             _distanceMatrix[i].resize(matrixSize, INT16_MAX);
@@ -27,15 +34,28 @@ public:
 
     /**
      * Replaces all the results for the given starting node with the given results vector.
-     * the whole vector is cleared before
-     * @param startingNode
-     * @param results
+     * the whole vector is cleared before moving the results vector data
+     * @param startingNodeIdx starting node of the paths in our results/destination distance vector
+     * @param results a vector which will be moved to this current results collection
      */
-    void replaceResultsCollection(size_t startingNode, std::vector<uint>& results) {
-        _distanceMatrix[startingNode].clear();
-        std::ranges::move(results, _distanceMatrix[startingNode].begin());
+    void replaceResultsCollection(size_t startingNodeIdx, std::vector<uint>& results) {
+        _distanceMatrix[startingNodeIdx].clear();
+        std::ranges::move(results, _distanceMatrix[startingNodeIdx].begin());
     }
 
+    /**
+     * Gives the shortest path time from a given node index to a given destination node index
+     * @param from origin node index
+     * @param to destination node index
+     * @return the distance of the shortest path between those two nodes
+     */
+    uint getShortestPath(size_t from, size_t to) { return _distanceMatrix[from][to]; }
+
+    /**
+     *
+     * @param idx Node index from
+     * @return The collection of distances to get to every graph node from the given index
+     */
     std::vector<uint>& operator[](size_t idx) { return _distanceMatrix[idx]; }
 };
 
