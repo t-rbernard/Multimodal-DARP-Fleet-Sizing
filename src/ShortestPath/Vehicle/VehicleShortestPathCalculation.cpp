ab5a5008bf6d716cb7f5a48139a4265084b0a7af
Romain BERNARD <romain.bernard@uca.fr>
2024-07-17 18:33:06 +0200

    implement Dijsktra to update existing distance matrix (TODO: lots of additional debug & allow computation of a matrix via edges)

--- src/ShortestPath/Vehicle/VehicleShortestPathCalculation.cpp
+++ src/ShortestPath/Vehicle/VehicleShortestPathCalculation.cpp
@@ -2,4 +2,45 @@
 // Created by romain on 16/07/24.
 //
 
+#include <queue>
 #include "VehicleShortestPathCalculation.h"
+#include "VehiclePathState.h"
+
+std::vector<uint> VehicleShortestPathCalculation::computeShortestPathsFromNode(const Graph& graph, size_t startingNodeIdx) {
+    std::vector<uint> results{graph.getShortestSaevPaths()[startingNodeIdx]};
+    std::priority_queue<VehiclePathState> stateQueue{};
+    stateQueue.emplace(startingNodeIdx, 0);
+
+    uint newDistance = INT32_MAX;
+    VehiclePathState currentState;
+    while(!stateQueue.empty()) {
+        currentState = stateQueue.top();
+        stateQueue.pop();
+        //Iterate over all possible nodes, as the graph is complete in the case of a distance matrix
+        for(size_t i = 0; i < results.capacity(); ++i) {
+            newDistance = currentState.getInstant() + graph.getShortestSAEVPath(currentState.getNodeIndex(), i);
+            if(newDistance < results[i]) {
+                stateQueue.emplace(i, newDistance);
+                results[i] = newDistance;
+            }
+        }
+    }
+
+    return results;
+}
+
+VehicleShortestPathContainer VehicleShortestPathCalculation::computeShortestPathsForGraph(const Graph &graph) {
+    std::vector<std::vector<uint>> results;
+    results.resize(graph.getNbNodes());
+    for(size_t i = 0; i < graph.getNbNodes(); ++i) {
+        results[i].resize(graph.getNbNodes());
+        std::ranges::move(computeShortestPathsFromNode(graph, i), results[i].begin());
+    }
+
+    return VehicleShortestPathContainer(results);
+}
+
+void VehicleShortestPathCalculation::computeAndUpdateShortestPathsForGraph(Graph &graph) {
+    VehicleShortestPathContainer results = computeShortestPathsForGraph(graph);
+    graph.setShortestSaevPaths(results.getDistanceMatrix());
+}
--- src/ShortestPath/Vehicle/VehicleShortestPathCalculation.h
+++ src/ShortestPath/Vehicle/VehicleShortestPathCalculation.h
@@ -6,7 +6,14 @@
 #define GREEDYALGORITHM_VEHICLESHORTESTPATHCALCULATION_H
 
 
+#include "VehicleShortestPathContainer.h"
+#include "../../instance/graph/Graph.h"
+
 class VehicleShortestPathCalculation {
+public:
+    static void computeAndUpdateShortestPathsForGraph(Graph& graph);
+    static VehicleShortestPathContainer computeShortestPathsForGraph(const Graph& graph);
+    static std::vector<uint> computeShortestPathsFromNode(const Graph& graph, size_t startingNodeIdx);
 
 };
 
