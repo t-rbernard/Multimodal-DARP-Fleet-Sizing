0c69d3348299b2f515f3c8be8d68d812975ae5b0
Romain BERNARD <romain.bernard@uca.fr>
2024-07-17 15:22:22 +0200

    init shortestPathContainer API

--- src/ShortestPath/Vehicle/VehicleShortestPathContainer.h
+++ src/ShortestPath/Vehicle/VehicleShortestPathContainer.h
@@ -6,8 +6,37 @@
 #define GREEDYALGORITHM_VEHICLESHORTESTPATHCONTAINER_H
 
 
-class VehicleShortestPathContainer {
+#include <vector>
+#include <cstdint>
+#include <algorithm>
 
+class VehicleShortestPathContainer {
+private:
+    std::vector<std::vector<uint>> _distanceMatrix{};
+public:
+    VehicleShortestPathContainer() = default;
+    VehicleShortestPathContainer(size_t matrixSize) {
+        _distanceMatrix.resize(matrixSize);
+        for(int i = 0; i < _distanceMatrix.capacity(); ++i)
+            _distanceMatrix[i].resize(matrixSize, INT16_MAX);
+    }
+
+    [[nodiscard]] const std::vector<std::vector<uint>> &getDistanceMatrix() const {
+        return _distanceMatrix;
+    }
+
+    /**
+     * Replaces all the results for the given starting node with the given results vector.
+     * the whole vector is cleared before
+     * @param startingNode
+     * @param results
+     */
+    void replaceResultsCollection(size_t startingNode, std::vector<uint>& results) {
+        _distanceMatrix[startingNode].clear();
+        std::ranges::move(results, _distanceMatrix[startingNode].begin());
+    }
+
+    std::vector<uint>& operator[](size_t idx) { return _distanceMatrix[idx]; }
 };
 
 
