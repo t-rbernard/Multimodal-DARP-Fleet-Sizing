8fd7806416b74c5cecd90df39279e2aabaedcb13
Romain BERNARD <romain.bernard@uca.fr>
2024-07-22 18:04:01 +0200

    add doc

--- src/ShortestPath/Vehicle/VehicleShortestPathCalculation.h
+++ src/ShortestPath/Vehicle/VehicleShortestPathCalculation.h
@@ -12,8 +12,24 @@
 
 class VehicleShortestPathCalculation {
 public:
+    /**
+     * Computes shortest paths from the given graph's distance matrix
+     * @param graph
+     */
     static void computeAndUpdateShortestPathsForGraph(Graph& graph);
+    /**
+     * Computes and returns an updated distance matrix from the given graph's distance matrix computed via Dijsktra's algorithm
+     * @param graph
+     * @return A matrix distances container
+     */
     static MatrixShortestPathContainer computeShortestPathsForGraph(const Graph& graph);
+    /**
+     * Computes the shortest paths to all the nodes in the graph from the given starting node.
+     * If a node is unreachable its default distance is INT32_MAX. Distance from the starting node to itself is 0
+     * @param graph
+     * @param startingNodeIdx
+     * @return a vector indexed by all the nodes with the sortest path's duration from the starting node to the destination node index
+     */
     static std::vector<uint> computeShortestPathsFromNode(const Graph& graph, size_t startingNodeIdx);
     /**
      * Returns an ordered set of closest PT SAV-compatible nodes from a given starting node index.
