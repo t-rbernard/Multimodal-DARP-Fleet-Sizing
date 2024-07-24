7e188dfd65c351902b1bfe1b5970418b2ceb70e7
Romain BERNARD <romain.bernard@uca.fr>
2024-07-24 18:11:58 +0200

    add graph preprocess example

---
+++ test/src/GraphPreprocessDebug.cpp
@@ -0,0 +1,20 @@
+//
+// Created by romain on 24/07/24.
+//
+
+#include "../lib/googletest/googletest/include/gtest/gtest.h"
+#include "../../src/ShortestPath/Vehicle/VehicleShortestPathCalculation.h"
+
+
+TEST(GraphPreprocessDebug, DebugBookInstance) {
+    std::string instancesPath = "../../resources/test/instances/graph preprocess/";
+    std::string instanceFolder = "book example/";
+    std::string graphDatFile = "graph.dat";
+
+    //Parse graph
+    Graph graph(instancesPath + instanceFolder + graphDatFile);
+    VehicleShortestPathCalculation::computeAndUpdateShortestPathsForGraph(graph);
+
+    std::string exportFolder = "../../resources/test/outputs/preprocess_debug/";
+    graph.exportGraphToFile(exportFolder + instanceFolder);
+}
