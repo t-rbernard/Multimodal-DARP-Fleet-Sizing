4093dc965aa5fb8336767306015fad16a9111e29
Romain BERNARD <romain.bernard@uca.fr>
2024-09-04 18:27:59 +0200

    create a bigger scale test for multimodal insertion heuristic debug purposes

--- test/src/MultimodalInsertionHeuristicDebug.cpp
+++ test/src/MultimodalInsertionHeuristicDebug.cpp
@@ -44,6 +44,40 @@ TEST(MultimodalInsertionHeuristicDebug, DebugBaseInstance) {
     std::cout << "------------------End multimodal insertion (entry)-------------------" << std::endl << std::endl;
 }
 
+TEST(MultimodalInsertionHeuristicDebug, DebugInstanceAlain) {
+    std::string instancesPath = "../../resources/test/instances/MultimodalHeuristic/";
+    std::string instanceFolder = "instance_alain_140624/";
+    std::string graphDatFile = "graph.dat";
+    std::string requestDatFile = "even_more_requests.dat";
+
+
+    //Parse graph
+    INIT_TIMER
+    Graph graphFromSingleFile(instancesPath + instanceFolder + graphDatFile);
+    graphFromSingleFile.computeAndUpdateShortestPathsMatrix(true);
+    std::vector<Request> requests = Request::getRequestsFromFile(instancesPath + instanceFolder + requestDatFile, graphFromSingleFile);
+
+    //Init instance
+    Instance instance(requests,graphFromSingleFile,4);
+    SAEVRoute routesContainer(graphFromSingleFile, requests, true);
+    SimpleModularHeuristic multimodalHeuristic(&graphFromSingleFile, &routesContainer, &requests);
+    STOP_TIMER("Instance parsing and init")
+    std::cout << "------------------Fin parsing instance et route-------------------" << std::endl << std::endl;
+
+    std::cout << "------------------Start preprocessings-------------------" << std::endl << std::endl;
+    START_TIMER
+    graphFromSingleFile.computeAndUpdateShortestTransitPaths();
+    STOP_TIMER("Preprocess")
+    std::cout << "------------------End preprocessings-------------------" << std::endl << std::endl;
+    std::cout << "------------------Start multimodal insertion (entry)-------------------" << std::endl << std::endl;
+    START_TIMER
+    for(size_t i = 0; i < multimodalHeuristic.getNbBaseRquests(); ++i) {
+        multimodalHeuristic.insertBestTransitEntryInRoute(requests[i], i);
+    }
+    STOP_TIMER("Multimodal insertion (entry)")
+    std::cout << "------------------End multimodal insertion (entry)-------------------" << std::endl << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
